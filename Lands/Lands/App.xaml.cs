d280eaf09924511163227a6e18042ee8dcb81036
Juan Pell <jcpell@gmail.com>
2019-02-02 12:30:49 -0300

    Hasta Parte 005

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace Lands
+{
+    using System;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+    using Views;
+
+    public partial class App : Application
+    {
+        #region Contructor
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new NavigationPage(new LoginPage());
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Lands.Infrastructure
+{
+    using ViewModels;
+
+
+    class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+        // Sirve para ligar la pag login a la mainviewmodel 
+        #region Constructor
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
