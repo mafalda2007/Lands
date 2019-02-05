0cce01b611772a7807d260e8abbf4f420b3e8f5d
Juan Pell <jcpell@gmail.com>
2019-02-05 13:48:01 -0300

    Add Models JSON

--- Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -1,27 +1,107 @@
 ﻿namespace Lands.ViewModels
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using GalaSoft.MvvmLight.Command;
     using System.Windows.Input;
+    using Views;
+    using Xamarin.Forms;
+   
 
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel 
     {
+
+        #region Atributes
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
-        public bool IsRemembered { get; set; }
+        public string Email
+        {
+            // Esto refresca el email. 
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+        public bool IsRemembered { get;set; }
+
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
         #endregion
         
         #region Contructors
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
+            // Para no escribir tanto en las pruebas
+            this.Email = "jcpell@gmail.com";
+            this.Password = "1234";
+
+            // http://restcountries.eu/rest/v2/all
         }
         #endregion
+
         #region Command
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+
+        private async void Login()
+        {
+            if(string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", "Ud. debe ingresar un email", "Aceptar");
+                return;
+            }
+            if(string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", "Ud. debe ingresar un password","Aceptar");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+            if(this.Email !="jcpell@gmail.com" || this.Password !="1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error", "Email o Password incorrecto", "Aceptar");
+                this.Password = string.Empty;
+                return;
+            }
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            this.Email = string.Empty;
+            this.Password = string.Empty;
+
+            MainViewModel.GetInstance().Lands = new LandsViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new LandsPage());
+        }
         #endregion
     }
 }
